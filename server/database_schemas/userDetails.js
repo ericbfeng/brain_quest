8fa45e9b2d460f6b25e8570ffa7e5384cfc8aeed
Rain Juhl <rainjuhl@DN0a1f68e4.SUNet>
2023-02-05 20:10:49 -0800

    Added occupation field. Yay.

--- server/database_schemas/userDetails.js
+++ server/database_schemas/userDetails.js
@@ -4,6 +4,7 @@ const UserDetailsSchema = new mongoose.Schema(
     {
         username: String,
         password: String,
+        occupation: String
     },
     {
         collection: "UserInfo",
