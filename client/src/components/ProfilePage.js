d3ca163eda0189f3c9b0f8bc388bbc183e560549
Rain Juhl <rainjuhl@DN0a1f6b7f.SUNet>
2023-03-14 23:49:18 -0700

    Made adding friends more user friendly, and it now also updates the page reactivly. Furthermore, I cleaned up obsolete code in the profile and server.

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -22,35 +22,34 @@ import '../styles/CommunityPage.css';
 
 
 
-function Userinfo(props){
+function FriendInfo(props){
+  let [friends, setFriends] = useState([]);
+  console.log("FRIEND INFO")
 
   useEffect(() => {
     // Update the document title using the browser API
-            
-      fetch(`/getfriends`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
+    fetch(`/getfriends`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to retrieve friends");
+        }
       })
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Failed to retrieve friends");
-          }
-        })
-        .then((data) => {
-          console.log(data);
-          // Do something with the response data
-        })
-        .catch((error) => {
-          console.error(error);
-          // Handle the error
-        });
+      .then((data) => {
+        console.log("DATA: ", data);
+        setFriends(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
 
-  });
-
+  }, []);
 
   return(
 
@@ -61,16 +60,16 @@ function Userinfo(props){
           <UserDash />
         </Grid>
         <Grid xs={4} sx={{ height: "100%" }}>
-          <FriendsTab tablabel={"Friends"} renderState={"accepted"}/>
+          <FriendsTab tablabel={"Friends"} friends={friends} setFriends={setFriends} renderState={"accepted"}/>
         </Grid>
         <Grid xs={8} sx={{ height: "100%" }}>
           <PastTest />
         </Grid>
-        <Grid xs={4} sx={{ height: "100%" }}>
-          <FriendsTab tablabel={"Pending Invites"} renderState={"pending"}/>
+        <Grid xs={6} sx={{ height: "100%" }}>
+          <FriendsTab tablabel={"Pending Invites"} friends={friends} setFriends={setFriends} renderState={"pending"}/>
         </Grid>
         <Grid xs={4} sx={{ height: "100%" }}>
-          <FriendsTab user={props.user} tablabel={"Add New Friends"} renderState={true}/>
+          <FriendsTab user={props.user} friends={friends}  setFriends={setFriends} tablabel={"Add New Friends"} renderState={true}/>
         </Grid>
       </Grid>
     </Stack>
@@ -81,162 +80,14 @@ function Userinfo(props){
 }
 
 function ProfilePage({userInfo, updateUser}) {  
-
-    // get the username of the current profile page we are viewing
-    let { pageUsername } = useParams();
-
-    let [friends, setFriends] = useState("Edit");
-  
-
-
-    let [username, setUsername] = useState(pageUsername);
-    let [password, setPassword] = useState();
-    let [occupation, setOccupation] = useState();
-    let [bEdit1, setbEdit1] = useState("Edit");
-    let [bEdit2, setbEdit2] = useState("Edit");
-    let [bEdit3, setbEdit3] = useState("Edit");
-    let record = userInfo.record;
-
-    // TODO: Grab current page username user's info from backend.
-    // for some reason, when I do it it is very inconsistent. The
-    // code below does not work when page is refreshed, for example
-    /*
-    const [pageUserData, setPageUserData] = useState();
-
-    // fetch the profile page user's data
-    useEffect(() => {
-      if(!pageUserData) {
-        fetch(`/getUser`, {
-          method: 'POST',
-          headers: {"Content-Type": "application/json"},
-          body: JSON.stringify({
-            userName: pageUsername
-          })
-        })
-        .then((res) => {
-          if(!res.ok) throw new Error(res.statusText);
-          else return res.json();
-        })
-        .then(data => setPageUserData(data))
-      }
-      
-    }, []);
-    */
-
-  
-
-    // boolean to denote if we are viewing our own profile or not
-    let isCurrUser = (pageUsername === userInfo.username);    
-
-    
-    if (!occupation) {occupation = "Unknown"};
-
-    const handleClick = (origin) => {
-      console.log('hi');
-      ReccomendFriends(0);
-      let update = false;
-
-      // Start by editing the the correct button state
-      switch (origin){
-        case "username":
-          if (bEdit1 === "Save") {update = true;}
-          bEdit1 === "Edit" ? setbEdit1("Save") : setbEdit1("Edit");
-          break;
-        case "password":
-          if (bEdit2 === "Save") {update = true;}
-          bEdit2 === "Edit" ? setbEdit2("Save") : setbEdit2("Edit");
-          break;
-        case "occupation":
-          if (bEdit3 === "Save") {update = true;}
-          bEdit3 === "Edit" ? setbEdit3("Save") : setbEdit3("Edit");
-          break;
-        default:
-          console.log("Error in handle click")
-          break;
-      }
-
-      // Update the users information if a save was clicked
-      if (update){
-        
-      }
-    }
-    
-    // Returns normal (username/password/occupation) text by default but
-    // returns an input field when editing (username/password/occupation) fields
-    function FieldText(props) {
-      let val = null;
-      let setVal = null;
-      switch (props.field){
-        case "username":
-          val = username;
-          setVal = setUsername;
-          break;
-        case "password":
-          val = password;
-          setVal = setPassword;
-          break;
-        case "occupation":
-          val = occupation;
-          setVal = setOccupation;
-          break;
-        default:
-          console.log("EditButton received invalid field");
-          break;
-      }
-
-      if (props.type === "Edit"){
-        // Bold the text if it is the username
-        if (props.field === "username"){
-          return (
-            <b>{val}</b>
-          )
-        } else{
-          return (
-            <> {val} </>
-          )
-        }
-      } else {
-        return (
-          <input placeholder={val} className="profile-input" value={val} onChange = {(e) => setVal(e.target.value)}/>
-        )
-      }
-
-    }
-
     return (
       <Stack spacing={2}>
         <div className="profile-header"> 
           <Link className="arrow-icon-container" to="/" >
             <BsArrowLeftShort className="arrow-icon" />
           </Link>
-          {/* <div className="profile-questions-container">
-            <img className="profile-picture" src="../default_profile_pic.jpeg" alt="../default_profile_pic.jpeg" />
-            <div className="profile-question-text"> Questions Answered: {userInfo.record.filter((value, index, array) => array.indexOf(value) === index).length} </div>
-          </div>
-          <div className="profile-container">
-            <div className="field-spacer"> 
-              <div className="profile-text"> Username: </div>
-              <FieldText type={bEdit1} field="username"/>
-            </div>
-            <button className="edit-button" onClick={() => handleClick("username")}> {bEdit1} </button>
-          </div>
-          <div className="profile-container">
-            <div className="field-spacer"> 
-              <div className="profile-text"> Password: </div>
-              <FieldText type={bEdit2} field="password"/>
-            </div>
-            <button className="edit-button" onClick={() => handleClick("password")}> {bEdit2} </button> 
-          </div>
-          <div className="profile-container">
-            <div className="field-spacer"> 
-              <div className="profile-text"> Occupation: </div>
-              <FieldText type={bEdit3} field="occupation"/>
-            </div>
-            <button className="edit-button" onClick={() => handleClick("occupation")}> {bEdit3} </button>
-          </div> */}
-        </div>
-      
-      <Userinfo user={userInfo.username}/>
+        </div> 
+      <FriendInfo user={userInfo.username}/>
       
       </Stack>
     );
